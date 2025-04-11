c4a5308919919a7a5b42dd53293444d04aa81c50
leonardosip <leonarddopereira2004@gmail.com>
2025-04-11 02:32:55 -0300

    criando o ERRO e estilizando a tela dele

--- src/pages/Erro.jsx
+++ src/pages/Erro.jsx
@@ -1,7 +1,16 @@
+import { Link } from "react-router-dom";
+import './erro.css'
+
 function Erro (){
     return (
-        <div>
-            <h1>  </h1>
+        <div className="not-found">
+
+            <h1> 404 </h1>
+
+            <div className="centro">
+            <h2>Pagina nao encontrada</h2>
+            <Link to="/" className="linq">Veja todos os filmes</Link>
+            </div>           
         </div>
     )
 }
