0ef5f15bf9e25d83de8888cdfecf2b7073b55403
leonardosip <leonarddopereira2004@gmail.com>
2025-04-14 03:07:42 -0300

    adicionando o botao de excluir e implementando

--- src/pages/Favoritos.jsx
+++ src/pages/Favoritos.jsx
@@ -1,33 +1,47 @@
 import './favoritos.css'
-import { useEffect , useState } from 'react';
+import { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
 
 
-function Favoritos(){
+function Favoritos() {
 
-    const [filmes , setFilmes ] = useState([]);
+    const [filmes, setFilmes] = useState([]);
 
     useEffect(() => {
         const minhaLista = localStorage.getItem("salvos")
-        setFilmes (JSON.parse(minhaLista) || [])
+        setFilmes(JSON.parse(minhaLista) || [])
     }, [])
 
+    function excluirFilme(id){
+        let filtroFilmes = filmes.filter ((item) => {
+            return (item.id !== id)
+        })
+
+        setFilmes(filtroFilmes);
+        localStorage.setItem('salvos' , JSON.stringify(filtroFilmes))
+    }
 
 
-    return(
+
+    return (
 
         <div>
             <h1> Meus Filmes </h1>
 
+            {filmes.length === 0 && <span className='aviso'> Voce nao possui nenhum filme salvo</span>}
+
             <ul>
                 {filmes.map((item) => {
-                    return(
+                    return (
                         <li key={item.id} className='area'>
                             <span className='titulo'> {item.title} </span>
 
                             <div className='botoes'>
                                 <Link to={`/filme/${item.id}`} className='botao1'> Ver Detalhes</Link>
-                                <button className='botao2'> Excluir </button>
+                                <button className='botao2' 
+                                onClick={() => excluirFilme(item.id)}>
+                                    Excluir
+                                </button>
                             </div>
                         </li>
                     )
